1044cd6fe770663e43475c51232d3c34e69785d6
Dushyant Khosla <dushyant.khosla@yahoo.com>
2025-11-26 20:06:30 +0400

    AI files 3

--- 07-AI/agent-financial-analysis/main.py
+++ 07-AI/agent-financial-analysis/main.py
@@ -1,6 +1,7 @@
 # import necessary python libraries
+import os
 from agno.agent import Agent
-from agno.models.xai import xAI
+from agno.models.openrouter import OpenRouter
 from agno.tools.yfinance import YFinanceTools
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.os import AgentOS
@@ -8,16 +9,21 @@ from agno.os import AgentOS
 # create the AI finance agent
 agent = Agent(
     name="xAI Finance Agent",
-    model = xAI(id="grok-beta"),
-    tools=[DuckDuckGoTools(), YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
+    model=OpenRouter(id="z-ai/glm-4.5-air:free",
+                 api_key=os.getenv("OPENROUTER_API_KEY")),
+    tools=[DuckDuckGoTools(), 
+           YFinanceTools()],
     instructions = ["Always use tables to display financial/numerical data. For text data use bullet points and small paragrpahs."],
     debug_mode = True,
     markdown = True,
     )
 
-# UI for finance agent
-agent_os = AgentOS(agents=[agent])
-app = agent_os.get_app()
+agent.print_response("""How has the NVIDIA stock been performing since August 2025""", stream=True)
 
-if __name__ == "__main__":
-    agent_os.serve(app="xai_finance_agent:app", reload=True)
+
+# # UI for finance agent
+# agent_os = AgentOS(agents=[agent])
+# app = agent_os.get_app()
+
+# if __name__ == "__main__":
+#     agent_os.serve(app="xai_finance_agent:app", reload=True)
